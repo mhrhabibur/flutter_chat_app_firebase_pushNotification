d6df0106655677bbfe48ff7846f73f5dfe9a2a95
Habibur Rahman <mhr.habibur@gmail.com>
2025-05-01 13:49:05 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/chat_app_firebase_push_notification/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chat_app_firebase_push_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Chat App")),
+        body: Center(child: Text("Chat App")),
+      ),
+    );
+  }
+}
